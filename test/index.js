64efe9ffda9616a7b26ede47f631153c276b02bd
Steffen Bruchmann <hi@sbruchmann.me>
2017-03-16 19:08:07 +0100

    feat(config): Use given options as configuration

--- test/index.js
+++ test/index.js
@@ -7,3 +7,8 @@ import defaultConfig from '../lib/default-config';
 test('Use default config when no options are given', () => {
   expect(new Jakob().config).to.deep.equal(defaultConfig);
 });
+
+test('Use given options as config', () => {
+  const jakob = new Jakob({root: __dirname});
+  expect(jakob.config.root).to.equal(__dirname);
+});
